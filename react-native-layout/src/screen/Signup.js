fb36238875a859e39516b8602c42084f6da02dc0
GarrellMac <gmacarilay1@gmail.com>
2026-02-17 14:32:58 +0800

    Created Signup screen structure

---
+++ react-native-layout/src/screen/Signup.js
@@ -0,0 +1,20 @@
+import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#F8F9FD',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 30,
+    },
+});
+
+export default function Signup() {
+    return (
+        <View style={styles.container}>
+
+        </View>
+    );
+}
